a34e72d81e7cded26cfa89d0101e0cc2bb9bc4a6
YUNJAEGOONER <coygyj@gmail.com>
2019-11-11 13:03:14 +0900

    Write the Constructor

--- MyHelloFrame.java
+++ MyHelloFrame.java
@@ -1,33 +1,26 @@
-
+import java.awt.*;
+import javax.swing.*;
 /**
  * Write a description of class MyHelloFrame here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author (2018315036 양유석 2018315039 이윤재)
+ * @version (2019.11.11)
  */
-public class MyHelloFrame
-{
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class MyHelloFrame
-     */
+public class MyHelloFrame extends JFrame
+{   
+    JLabel jl;
+    JPanel jp;
     public MyHelloFrame()
     {
-        // initialise instance variables
-        x = 0;
+        this.setSize(400,400);
+        this.setVisible(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("실습_3(20191111)");
+        jp = new JPanel(); //새로운 패널 생성
+        jl = new JLabel("HELLO"); //새로운 레이블 생성
+        jp.add(jl);// 패널에 레이블 추가하기
+        addMouseListener(new MyHelloPanelListener(jl));
+        addKeyListener(new MyHelloPanelListener(jl));
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
 }
