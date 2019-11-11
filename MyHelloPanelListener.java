657a4e3033abb4d7bd6575f2e96b53bbde29838a
FWSU <yys9905@naver.com>
2019-11-11 13:04:18 +0900

    change Class name

---
+++ MyHelloPanelListener.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
+/**
+ * Write a description of class MyHelloFrameListener here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class MyHelloPanelListener extends MouseAdpater implements KeyListener
+{
+    JLabel jl;
+    public MyHelloPanelListener(JLabel jl){
+        
+    }
+}
