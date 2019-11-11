224416f5b53339ff0122895a0f3f1896f6e7a5f7
FWSU <yys9905@naver.com>
2019-11-11 13:15:53 +0900

    delete Panel write add mathod

--- MyHelloFrame.java
+++ MyHelloFrame.java
@@ -8,21 +8,14 @@ import javax.swing.*;
  */
 public class MyHelloFrame extends JFrame
 {   
-    JLabel jl;
-    JPanel jp;
     public MyHelloFrame()
     {
         this.setSize(400,400);
         this.setVisible(true);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle("실습_3(20191111)");
-        jp = new JPanel(); //새로운 패널 생성
-        jl = new JLabel("HELLO"); //새로운 레이블 생성
-        jp.add(jl);// 패널에 레이블 추가하기
-        this.add(jp);
-        MyHelloPanelListener ml = new MyHelloPanelListener(jl);
-        addMouseListener(ml);
-        addKeyListener(ml);
+        MyHelloPanelListener ml = new MyHelloPanelListener();
+        this.add(ml);
     }
 
 }
